fb5dae937be284869f8ff09f2dad0cb12fb37283
Mikołaj Rod <mikolaj.rodkiewicz@gmail.com>
2018-01-28 23:32:42 +0100

    add ScrollView to main activity, delete NotificationPublisher and add Notifier

--- app/src/main/java/mrodkiewicz/pl/niedzielehandlowe/MainActivity.java
+++ app/src/main/java/mrodkiewicz/pl/niedzielehandlowe/MainActivity.java
@@ -1,14 +1,11 @@
 package mrodkiewicz.pl.niedzielehandlowe;
 
-import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -23,32 +20,38 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
-
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import mrodkiewicz.pl.niedzielehandlowe.helpers.Data;
 import mrodkiewicz.pl.niedzielehandlowe.decoratos.CloseSundayDecorator;
 import mrodkiewicz.pl.niedzielehandlowe.decoratos.OpenWeekendDecorator;
 import mrodkiewicz.pl.niedzielehandlowe.decoratos.TodayDecorator;
-import mrodkiewicz.pl.niedzielehandlowe.helpers.NotificationPublisher;
+import mrodkiewicz.pl.niedzielehandlowe.helpers.Data;
+import mrodkiewicz.pl.niedzielehandlowe.helpers.Notifier;
 
 
 public class MainActivity extends AppCompatActivity {
-    private String TAG = getClass().getSimpleName()+ " flag";
+    private String TAG = getClass().getSimpleName() + " flag";
     public static Data data = new Data();
 
-    @BindView(R.id.textView) TextView textView;
-    @BindView(R.id.calendarView) MaterialCalendarView calendarView;
-    @BindView(R.id.imageView) ImageView imageView;
-    @BindView(R.id.linearLayout) LinearLayout linearLayout;
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.calendarView)
+    MaterialCalendarView calendarView;
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.linearLayout)
+    LinearLayout linearLayout;
     private SharedPreferences preferences;
-    private int openSundayColor,closeSundayColor,openSundayCalendarColor,closeSundayCalendarColor;
+    private Notifier notifier;
+    private NotificationCompat.Builder notification;
+    private int openSundayColor, closeSundayColor, openSundayCalendarColor, closeSundayCalendarColor;
 
 
     @Override
@@ -60,19 +63,24 @@ public class MainActivity extends AppCompatActivity {
 
 
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        notifier = new Notifier();
+        notifier.init(this);
+
 
         openSundayColor = getResources().getColor(R.color.openSunday);
         closeSundayColor = getResources().getColor(R.color.closeSunday);
         openSundayCalendarColor = getResources().getColor(R.color.openSundayCalendar);
         closeSundayCalendarColor = getResources().getColor(R.color.closeSundayCalendar);
 
+        notification = new NotificationCompat.Builder(this);
+
         ArrayList<CalendarDay> closeSundaysCollection = new ArrayList<>();
         Calendar calendar = Calendar.getInstance();
-        for (LocalDate localDate:data.dateCloseList){
-            calendar.set(localDate.getYear(),localDate.getMonthOfYear() - 1,localDate.getDayOfMonth());
+        for (LocalDate localDate : data.dateCloseList) {
+            calendar.set(localDate.getYear(), localDate.getMonthOfYear() - 1, localDate.getDayOfMonth());
             CalendarDay day = CalendarDay.from(calendar);
             closeSundaysCollection.add(day);
-            Log.d(TAG,day.toString());
+            Log.d(TAG, day.toString());
         }
 
 
@@ -89,20 +97,20 @@ public class MainActivity extends AppCompatActivity {
         );
 
 
-        if (data.isNextSundayClose()){
-            if (Days.daysBetween(data.getClosestSunday(),data.getToday()).getDays() == 0){
+        if (data.isNextSundayClose()) {
+            if (Days.daysBetween(data.getClosestSunday(), data.getToday()).getDays() == 0) {
                 textView.setText(getString(R.string.state_today_close_text));
-            }else {
+            } else {
                 textView.setText(getString(R.string.state_close_text));
             }
             textView.setTextColor(closeSundayColor);
             imageView.setImageResource(R.drawable.ic_remove_shopping_cart_black_24dp);
             linearLayout.setBackgroundColor(closeSundayColor);
 
-        }else{
-            if (Days.daysBetween(data.getClosestSunday(),data.getToday()).getDays() == 0){
+        } else {
+            if (Days.daysBetween(data.getClosestSunday(), data.getToday()).getDays() == 0) {
                 textView.setText(getString(R.string.state_today_open_text));
-            }else {
+            } else {
                 textView.setText(getString(R.string.state_open_text));
             }
             textView.setTextColor(openSundayColor);
@@ -113,40 +121,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void scheduleNotification(Notification notification, int delay) {
-        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
-        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
-        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        long futureInMillis = SystemClock.elapsedRealtime() + delay;
-        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
-    }
-
-    private Notification getNotification(String content) {
-        Notification.Builder builder = new Notification.Builder(this);
-        builder.setContentTitle(getString(R.string.app_name));
-        builder.setContentText(content);
-        builder.setSmallIcon(R.drawable.ic_launcher_background);
-        return builder.build();
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent;
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_settings:
-                intent = new Intent(this,SettingsViewerActivity.class);
+                intent = new Intent(this, SettingsViewerActivity.class);
                 startActivity(intent);
                 return true;
             case R.id.action_bug:
-                scheduleNotification(getNotification("5 second delay"), 5000);
+                notifier.setAlarm(new Date());
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/mrodkiewicz/pl/niedzielehandlowe/helpers/Notifier.java
@@ -0,0 +1,31 @@
+package mrodkiewicz.pl.niedzielehandlowe.helpers;
+
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import java.util.Date;
+
+public class Notifier extends BroadcastReceiver{
+    private AlarmManager alarmManager;
+    private Intent intent;
+    private PendingIntent pendingIntent;
+
+    public void init(Context context){
+        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        intent = new Intent(context,Notifier.class);
+        pendingIntent = PendingIntent.getBroadcast(context,12112,intent,0);
+    }
+    public void setAlarm(Date date){
+        alarmManager.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis() + 5 * 1000,pendingIntent);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm....", Toast.LENGTH_LONG).show();
+    }
+}
